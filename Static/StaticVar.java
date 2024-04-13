786d84b44e7ea8068866aaf614bc9b761e8a630c
bmstylish <assassins1458@gmail.com>
2024-04-14 01:22:18 +1200

    Update (static)

---
+++ Static/StaticVar.java
@@ -0,0 +1,29 @@
+package Static;
+
+class Book {
+    private String title;
+    private double price;
+    static String pub = "Test Publisher";
+
+    public Book(String title, double price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getInfo() {
+        return "Title: " + title + ", Price: " + price + ", Publisher: " + pub;
+    }
+}
+
+public class StaticVar {
+    public static void main(String[] args) {
+        Book bk1 = new Book("Java", 89.9);
+        Book bk2 = new Book("Test", 129.9);
+        // Book.pub = "No Pub;"
+        // Static field should be accessed in a static way
+        bk1.pub = "No Pub";
+        System.out.println(bk2.getInfo()); // Changing bk1, changed Pub of all Book class
+
+    }
+
+}
---
+++ Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String args[]) {
+        for (int x = 0; x < args.length; x++) {
+            System.out.println(args[x]);
+        }
+    }
+}
